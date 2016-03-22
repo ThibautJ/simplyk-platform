c0f4d2b175c56544e5f767b08223262c232150d8
ThibautJ <thibaut.jaurou@gmail.com>
2016-03-22 11:36:10 -0400

    Code cleaned and basic auth implemented
    All the code has been modified and cleaned. A basic auth system is OP

--- app.js
+++ app.js
@@ -1,69 +1,129 @@
+//Requires
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieSession = require('cookie-session')
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var socket_io    = require( "socket.io" );
+var sessions = require('client-sessions');
+var mongoose = require('mongoose');
 
-var users = require('./routes/users');
-var profile = require('./routes/profile');
-var connexioncitoyen = require('./routes/connexioncitoyen');
-var creationcitoyen = require('./routes/creationcitoyen');
-var map = require('./routes/map');
-var accueil = require('./routes/accueil');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var User = mongoose.model('User', new Schema({
+	id: ObjectId,
+	fname: String,
+	lname: String,
+	mail: {type: 	String, unique: true },
+	age: String,
+}));
 
 var app = express();
 
-// Socket.io
-var io = socket_io();
-app.io = io;
+//Connect to mongo
+mongoose.connect('mongodb://localhost/newauth');
 
+//middlewares
+app.use(express.static('C:/Users/Thibaut/Documents/Code/Node.js/simplyk/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(sessions({
+	cookieName: 'session',
+	secret: 'nopeacasvf51355fe4a6«sc4fesv43«s1cd35f86rg45f',
+	duration: 30 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000,
+}));
 
-var routes = require('./routes/index')(io);
+app.set('view engine', 'jade');
+app.locals.pretty = true;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cookieSession({secret : 'simplyksession'}))
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'models')));
-
-app.use('/', routes);
-app.use('/users', users)
-.use('/profile', profile)
-.use('/creationcitoyen', creationcitoyen)
-.use('/connexioncitoyen', connexioncitoyen)
-.use('/map', map)
-.use('/accueil', accueil)
-
-.use(function(req, res, next){
-    if (typeof(req.session.username) == 'undefined') {
-        req.session.username = "default";
-    }
-    next();
-})
-
-// socket.io events
-io.on( "connection", function( socket )
-{
-    console.log( "A user connected" );
+//router
+app.get('/', function(req, res){
+	res.render('accueil.jade');
 });
 
-io.on("connection", function(socket){
-	socket.emit('misss', {content: ' eh oui gradur ! ', importance: '1'})
+app.get('/connexioncitoyen', function(req, res){
+	res.render('connexioncitoyen.jade');
+});
+
+app.post('/connexioncitoyen', function(req, res){
+	if(req.session){
+		console.log ('req.body.mail : ' + req.body.mail);
+		console.log ('req.body.lname : ' + req.body.lname);
+		User.findOne({ mail: req.body.mail}, function(err, user){
+			if(!user){
+				var error = "Wrong mail";
+				console.log (error + '1');
+				res.render('connexioncitoyen.jade', {error: error});
+			} else {
+				if(req.body.lname === user.lname){
+					req.session.user = user; //set-cookie...
+					res.redirect('/map');
+				} else {
+					var error = "Wrong mail";
+					console.log (error + '2');
+					res.render('connexioncitoyen.jade', {error: error});
+				}
+			}
+		});
+	} else {
+		res.redirect('/connexioncitoyen');
+	}
 })
 
+app.get('/creationcitoyen', function(req, res){
+	res.render('creationcitoyen.jade');
+});
+
+app.post('/creationcitoyen', function(req, res){
+	var user = new User({
+		fname: req.body.fname,
+		lname: req.body.lname,
+		age: req.body.age,
+		mail: req.body.mail,
+	});
+	user.save(function(err){
+		if(err){
+			var error = 'Something bad happened !'
+			if(err.code === 11000){
+				var error = 'This email is already used !'
+			}
+			res.render('creationcitoyen.jade', {error: error})
+		} else{
+			res.redirect('/map');
+		}
+	})
+})
+
+app.get('/map', function(req, res){
+	if(req.session && req.session.user){
+		User.findOne({ email: req.session.email }, function(err, user){
+			if(!user){
+				req.session.reset();
+				res.redirect('/');
+			} else {
+				res.locals.user = user;
+				res.render('map.jade');
+			}
+		})
+	} else {
+		res.redirect('/');
+	}
+});
+
+app.post('/map', function(req, res){
+
+});
+
+app.get('/profile', function(req, res){
+	if(req.session && req.session.user){
+		res.locals.user = req.session.user;
+		res.render('profile.jade');
+	} else {
+		res.redirect('/');
+	}
+});
+
+app.get('/logout', function(req, res){
+	req.session.reset();
+	res.redirect('/');
+});
 
 
-module.exports = app;
+app.listen(3000);
--- models/database.js
+++
@@ -1,7 +0,0 @@
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/users';
-
-var client = new pg.Client(connectionString);
-client.connect();
-var query = client.query('CREATE TABLE users(id SERIAL PRIMARY KEY, nom VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', function() { client.end(); });
