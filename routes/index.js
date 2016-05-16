24d2fbd43793d1f235912d78ee765183c1c72b46
Arrowana <pierre.duval@gadz.org>
2016-05-17 00:06:04 +0800

    "works" on map page

--- routes/index.js
+++ routes/index.js
@@ -40,21 +40,19 @@ router.get('/map', stormpath.getUser, stormpath.loginRequired, function(req, res
             for(var i in opps)
             {
                 var opp = opps[i];
-                console.log(opp);
 
-                for(var j in opp.candidates)
+                console.log(req.user.customData.favopps);
+                console.log(opp._id);
+                console.log(req.user.customData.favopps.indexOf(opp._id));
+                if(req.user.customData.favopps.indexOf(opp._id.toString()) > -1)
                 {
-                    var candidate = opp.candidates[j];
-                    if(candidate[req.user.email])
-                    {
-                        console.log('applied for this one');
-                        opp.status = candidate[req.user.email].status;
-                        console.log(opp.status);
-                        break;
-                    }
+                    console.log('User inscrit');
+                    opp.status = "Pending";
+                } 
+                else
+                {
+                    opp.status = "Open";
                 }
-                
-                if(opp.status != "Pending") opp.status = "Not applied";
             }
 
 			res.render('map.jade', {opps: opps, session: req.session});
@@ -95,25 +93,24 @@ router.post('/addopp', stormpath.getUser, function(req,res){
 router.post('/addfavopp', stormpath.loginRequired, stormpath.getUser, function(req,res){
     console.log('Enter addfavopp');
 
-	if(!Array.isArray(req.user.customData.favopps)){
-		req.user.customData.favopps = [];
-	}
-
 	//identifiant de l'opp sur laquelle on a cliqué
 	var id_favorite=req.body.identifiant;
 
     console.log(id_favorite);
     //console.log(req.user.customData.favopps);
 
+	if(!Array.isArray(req.user.customData.favopps)){
+        console.log('set array');
+		req.user.customData.favopps = [];
+	}
+
     console.log('request');
     //On retrouve le contenu de l'opp via la bdd
-	opp = models.Opp.findOne({id: ObjectId(id_favorite)}, {}, function(err, found_opp){
+	models.Opp.findOne({id: ObjectId(id_favorite)}, {}, function(err, found_opp){
 		if (err) return handleError(err);
             //Create opps list
-            console.log('found_opp: '+ found_opp); 
             console.log('found_opp: ' + found_opp.candidates);
         
-            console.log(req.user);
             var candidature = {};
             candidature[req.user.email] = {status: "Pending"};
 
@@ -131,29 +128,28 @@ router.post('/addfavopp', stormpath.loginRequired, stormpath.getUser, function(r
                     console.log(err)
                 }
             });
+
+            //On regarde si ce mandat est deja dans les favoris.
+            //Si le mandat est déjà dans les favoris, on le supprime.
+            if(req.user.customData.favopps[req.user.customData.favopps.indexOf(found_opp._id)]){
+                console.log("deja candidat");
+            }
+            else{
+                console.log("candidate pour le mandat");
+                req.user.customData.favopps.push(found_opp._id);
+            }
+
+            req.user.customData.save(function (err) {
+                if (err) {
+                    res.status(400).end('Oops!  There was an error: ' + err.userMessage);
+                }else{
+                    console.log('Name was changed!');
+                }
+            });
 	});
 
     //Modified to comply (a bit) with https://docs.mongodb.com/v3.0/tutorial/model-referenced-one-to-many-relationships-between-documents/
 
-    //On regarde si ce mandat est deja dans les favoris.
-    //Si le mandat est déjà dans les favoris, on le supprime.
-    if(req.user.customData.favopps.indexOf(opp._id)){
-        console.log("le mandat est deja dans les favoris : on le supprime");
-        //I read that delete is for sure a bad idea then what is the solution ?!?
-        delete req.user.customData.favopps[req.user.customData.favopps.indexOf(opp._id)];
-    }
-    else{
-        console.log("le mandat n'est pas encore dans les favoris : on le rajoute");
-        req.user.customData.favopps.push(opp._id);
-    }
-
-	req.user.customData.save(function (err) {
-		if (err) {
-			res.status(400).end('Oops!  There was an error: ' + err.userMessage);
-		}else{
-			console.log('Name was changed!');
-		}
-	});
 
 	console.log('out addfavapp');
 	res.end();
